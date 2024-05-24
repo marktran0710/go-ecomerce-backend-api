78f3b18118b5e05d6f2a509da2b54868d2289299
Hau Tran <tranleconghau772001@gmail.com>
2024-05-24 23:40:42 +0700

    gin continue

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -8,10 +8,12 @@ import (
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
+	r.GET("/ping", Pong)
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
+
+func Pong(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "pong...ping",
+	})
 }
