3223d3c44275ee5e4591aa063c2f038c662d1778
Hau Tran <tranleconghau772001@gmail.com>
2024-05-25 16:24:18 +0700

    update main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -8,7 +8,16 @@ import (
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", Pong)
+
+	v1 := r.Group("v1/group")
+	{
+		v1.GET("/ping", Pong)
+		v1.PUT("/ping", Pong)
+		v1.PATCH("/ping", Pong)
+		v1.DELETE("/ping", Pong)
+		v1.HEAD("/ping", Pong)
+		v1.OPTIONS("/ping", Pong)
+	}
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
 
