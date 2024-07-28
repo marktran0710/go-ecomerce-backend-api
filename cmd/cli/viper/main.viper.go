4c23a7df4ba5c0efde21b27df252ac080f0f6e26
hautran <tranleconghau772001@gmail.com>
2024-07-28 10:53:10 +0700

    using viper marshal with yaml

--- cmd/cli/viper/main.viper.go
+++ cmd/cli/viper/main.viper.go
@@ -6,6 +6,21 @@ import (
 	"github.com/spf13/viper"
 )
 
+type Server struct {
+	Port int `mapstructure:"port"`
+}
+
+type Databases struct {
+	User     string `mapstructure:"user"`
+	Password string `mapstructure:"password"`
+	Host     string `mapstructure:"host"`
+}
+
+type Config struct {
+	Server    Server
+	Databases []Databases `mapstructure:"database"`
+}
+
 func main() {
 	viper := viper.New()
 	viper.AddConfigPath("./config/")
@@ -18,4 +33,15 @@ func main() {
 	}
 
 	fmt.Println("Server Port::", viper.GetInt("server.port"))
+
+	var config Config
+	if err = viper.Unmarshal(&config); err != nil {
+		panic(fmt.Errorf("Unable to decode configuration %w\n", err))
+	}
+
+	fmt.Println("Config Port::", config.Server.Port)
+	for _, value := range config.Databases {
+		fmt.Printf("Databases user: %s, password: %s, host: %s \n", value.User, value.Password, value.Host)
+	}
+
 }
