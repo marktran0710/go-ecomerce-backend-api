7a3cfcc4cd2d2343e2f0c8df69c9683767363c3b
hautran <tranleconghau772001@gmail.com>
2024-08-06 22:36:04 +0700

    set pool configuration

--- internal/initialize/mysql.go
+++ internal/initialize/mysql.go
@@ -2,21 +2,44 @@ package initialize
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/marktran77/go-ecomerce-backend-api/global"
+	"go.uber.org/zap"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
+
+func checkErrorPanic(err error, errString string) {
+	if err != nil {
+		global.Logger.Error(errString, zap.Error(err))
+		panic(err)
+	}
+}
 
 func main() {
 	m := global.Config.Mysql
 	// refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
 	dsn := "%s:%s@tcp(%s:%v)/%s?charset=utf8mb4&parseTime=True&loc=Local"
 	var s = fmt.Sprintf(dsn, m.Username, m.Password, m.Host, m.Port, m.Dbname)
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(s), &gorm.Config{
+		SkipDefaultTransaction: false,
+	})
+	checkErrorPanic(err, "InitMysql initialization error")
+	global.Logger.Info("Initializing MySql Successfully")
+	global.Mdb = db
+
 }
 func SetPool() {
+	m := global.Config.Mysql
+	sqlDb, err := global.Mdb.DB()
+	if err != nil {
+		fmt.Println("mysql error %s::", err)
+	}
 
+	sqlDb.SetConnMaxIdleTime(time.Duration(m.MaxIdleConns))
+	sqlDb.SetMaxOpenConns(m.MaxOpenConns)
+	sqlDb.SetConnMaxLifetime(time.Duration(m.ConnMaxLifeTIme))
 }
 
 func migrateTables() {
