3f35cd501cdb262c55f6bc44dfe7006523eb2628
hautran <tranleconghau772001@gmail.com>
2024-08-08 23:27:42 +0700

    add docker-compose file

--- internal/initialize/redis.go
+++ internal/initialize/redis.go
@@ -1,3 +1,17 @@
 package initialize
 
-func InitRedis() {}
+import (
+	"context"
+
+	"github.com/redis/go-redis/v9"
+)
+
+var ctx = context.Background()
+
+func InitRedis() {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+}
