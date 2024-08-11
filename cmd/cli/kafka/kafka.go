258f0797e7ce16a9d1563668d3d9a81b3f080f37
hau tran <tranleconghau772001@gmail.com>
2024-08-11 18:09:06 +0700

    init kafka

---
+++ cmd/cli/kafka/kafka.go
@@ -0,0 +1,28 @@
+package kafka
+
+import (
+	"strings"
+
+	kafka "github.com/segmentio/kafka-go"
+)
+
+// for producer
+func getKafkaWriter(kafkaURL, topic string) *kafka.Writer {
+	return &kafka.Writer{
+		Addr:     kafka.TCP(kafkaURL),
+		Topic:    topic,
+		Balancer: &kafka.LeastBytes{},
+	}
+}
+
+// for consumer
+func getKafkaReader(kafkaURL, topic, groupID string) *kafka.Reader {
+	brokers := strings.Split(kafkaURL, ",")
+	return kafka.NewReader(kafka.ReaderConfig{
+		Brokers:  brokers,
+		GroupID:  groupID,
+		Topic:    topic,
+		MinBytes: 10e3, // 10KB
+		MaxBytes: 10e6, // 10MB
+	})
+}
