efd21dd50eab3b021f2f4b418ef31cc9a7985365
hau tran <tranleconghau772001@gmail.com>
2024-09-18 07:21:52 +0700

    hash email for redis and otp

--- internal/services/user.services.go
+++ internal/services/user.services.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"github.com/marktran77/go-ecomerce-backend-api/internal/repo"
+	"github.com/marktran77/go-ecomerce-backend-api/internal/utils/crypto"
 	"github.com/marktran77/go-ecomerce-backend-api/internal/utils/random"
 	"github.com/marktran77/go-ecomerce-backend-api/pkg/response"
 )
@@ -28,6 +29,8 @@ func NewUserService(userRepo repo.IUserRepo, userAuth repo.IUserAuthRepo) IUserS
 // Register implements IUserService.
 func (us *UserService) Register(email string, purpose string) int {
 	// 0. hashEmail
+	hashEmail := crypto.GetHash(email)
+	fmt.Printf("hashEmail::%s\n", hashEmail)
 
 	// 5. check OTP is available
 
@@ -47,7 +50,7 @@ func (us *UserService) Register(email string, purpose string) int {
 	fmt.Printf("Otp is :::%d\n", otp)
 
 	// 3. save OPT in Redis with expiration time
-	err := us.userAuthRepo.AddOTP(email, otp, int64(10*time.Minute))
+	err := us.userAuthRepo.AddOTP(hashEmail, otp, int64(10*time.Minute))
 	if err != nil {
 		return response.ErrInvaildOTP
 	}
